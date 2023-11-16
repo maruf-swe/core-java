5e4a050c8e6396cd143905fc74af1b730758c78f
maruf-py <marufur1847@gmail.com>
2023-11-17 03:59:25 +0600

    add java Variables files

---
+++ Java Variables/JavaVariables.java
@@ -0,0 +1,27 @@
+package java_variables;
+
+public class JavaVariables {
+    public static void main(String[] args) {
+        // String Variable. stores text
+        String name = "Marufur Rahman";
+
+        // int Variable. stores integers without decimal
+        int number = 10;
+
+        // double variable. stores floating point numbers
+        double num = 1.1;
+
+        // char variable. Store characters such as 'a' or 'B' with single quotes
+        char a = 'a';
+
+        // bolean variables is True or Fales
+        boolean aa = true;
+        boolean ff = false;
+
+        // print variables value
+        System.out.println(aa);
+        System.out.println(number);
+
+    }
+
+}
