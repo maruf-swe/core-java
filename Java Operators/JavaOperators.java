549ed1435b6f65cde99fd9ef0f87d79bfe37e022
Marufur Rahman <59738629+maruf-py@users.noreply.github.com>
2023-11-17 04:54:20 +0600

    added JavaOperators.java

---
+++ Java Operators/JavaOperators.java
@@ -0,0 +1,17 @@
+package java_operators;
+//Java Operators are used to perform operations on variables and values.
+public class JavaOperators {
+    public static void main(String[] args) {
+
+        /*
+            types of operators:
+            Arithmetic operators: +,-,*,/,%
+            Assignment operators: =, +=,-=, *=,
+            Comparison operators: <,>, ==, !=, >=, <=
+            Logical operators: && Logical and(return if both statement true, || logical or, logical not
+            Bitwise operators
+            */
+
+    }
+
+}
