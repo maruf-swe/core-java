dbf91f2b194314d3e9885c9218c263f04eb723a5
Marufur Rahman <59738629+maruf-py@users.noreply.github.com>
2023-11-18 03:25:07 +0600

    Update SwitchCase.java

--- Switch Case/SwitchCase.java
+++ Switch Case/SwitchCase.java
@@ -1,5 +1,13 @@
 package switch_case;
-
+/*
+This is how it works:
+The switch expression is evaluated once.
+The value of the expression is compared with the values of each case.
+If there is a match, the associated block of code is executed.
+The break and default keywords are optional, and will be described later in this chapter
+When Java reaches a break keyword, it breaks out of the switch block.
+The default keyword specifies some code to run if there is no case match:
+*/
 public class SwitchCase {
     public static void main(String[] args) {
         int a = 2;
