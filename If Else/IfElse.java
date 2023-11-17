df5ae1701ea888f02c852e213a7a5d8a6f58419b
Marufur Rahman <59738629+maruf-py@users.noreply.github.com>
2023-11-18 03:01:20 +0600

    Create IfElse.java

---
+++ If Else/IfElse.java
@@ -0,0 +1,45 @@
+package if_else;
+
+public class IfElse {
+    public static void main(String[] args) {
+        /*
+        Java Conditions and If Statements
+        Less than: a < b
+        Less than or equal to: a <= b
+        Greater than: a > b
+        Greater than or equal to: a >= b
+        Equal to a == b
+        Not Equal to: a != b
+        # Note that if is in lowercase letters
+        */
+
+        //# Example of If
+        int a = 20, b = 10;
+        if (a > b) {
+            System.out.println("20 is grater");
+        }
+
+        //2nd example of if
+        if (100 < 200) {
+            System.out.println("200 is grater than 100");
+        }
+
+        // example of if else
+        int time = 20;
+        if (time < 18) {
+            System.out.println("Good day.");
+        } else {
+            System.out.println("Good evening.");
+        }
+
+        // example of else if
+        int num = 20;
+        if (num < 18) {
+            System.out.println("Good day.");
+        } else if (num > 18) {
+            System.out.println("Good evening.");
+        } else {
+            System.out.println("Good Night");
+        }
+    }
+}
