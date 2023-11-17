f265505b315c2c3a784a9e105d7c3b5126928caa
Marufur Rahman <59738629+maruf-py@users.noreply.github.com>
2023-11-18 03:15:44 +0600

    Create ShortIfElse.java

---
+++ If Else/ShortIfElse.java
@@ -0,0 +1,15 @@
+package if_else;
+
+public class ShortIfElse {
+    public static void main(String[] args) {
+
+        /*
+            Syntax of Short Hand If Else
+            variable = (condition) ? expressionTrue :  expressionFalse;
+        */
+
+        int a = 10;
+        String number = (a < 18) ? "Good Day" : "Good Night";
+        System.out.println(number);
+    }
+}
