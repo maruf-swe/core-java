5836f647823f8cbfcdeac19c1b08bfedd0c9960b
Marufur Rahman <59738629+maruf-py@users.noreply.github.com>
2023-11-18 06:28:52 +0600

    Create BasicArray.java

---
+++ Array/BasicArray.java
@@ -0,0 +1,28 @@
+package array;
+
+/*
+    Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+    To declare an array, define the variable type with square brackets:
+
+ */
+public class BasicArray {
+    public static void main(String[] args) {
+        // declare Array of String
+        String[] cars = {"BMW", "Mercedes", "Volvo"};
+
+        // declare array of Int
+
+        int[] numbers = {1,2,3,4,5};
+
+
+        // Access the element of Array
+        System.out.println(cars[1]);
+
+        // Change the Array element
+        numbers[2] = 10;
+        System.out.println(numbers[2]);
+
+        // Array of Length
+        System.out.println(numbers.length);
+    }
+}
