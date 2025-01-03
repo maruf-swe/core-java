5af64d9e3801d84f35fd9096c7660194a3f315aa
Maruf <marufur1847@gmail.com>
2025-01-03 23:50:32 +0600

    Basics folder first file adding

---
+++ basics/01-introduction/HelloMaruf.java
@@ -0,0 +1,19 @@
+package introduction;
+
+public class HelloMaruf {
+    public static void main(String[] args) {
+        System.out.println("Hello Maruf");
+    }
+}
+
+
+// Single line Comment
+// System.out.println("This is an comment.");
+
+//
+
+/*
+    Multi-line Comment
+    System.out.println("This is the first line comment.");
+    System.out.println("This is the second line comment.");
+*/
