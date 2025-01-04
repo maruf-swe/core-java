97f887c46b2619b1da0ac07d1f5e6e7bbd34072a
Maruf <marufur1847@gmail.com>
2025-01-05 02:54:50 +0600

    add variables and local variables

---
+++ basics/01-introduction/Comments.java
@@ -0,0 +1,17 @@
+package introduction;
+
+public class Comments {
+    public static void main(String[] args) {
+
+        // Single line Comment
+        // System.out.println("This is an comment.");
+
+        //
+
+        /*
+           Multi-line Comment
+           System.out.println("This is the first line comment.");
+           System.out.println("This is the second line comment.");
+         */
+    }
+}
