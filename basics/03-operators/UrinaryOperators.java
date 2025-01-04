ae3bbe67b397191350d10738387ac9f9a9c473c8
Maruf <marufur1847@gmail.com>
2025-01-05 04:22:02 +0600

    add Operators

---
+++ basics/03-operators/UrinaryOperators.java
@@ -0,0 +1,18 @@
+package operators;
+
+public class UrinaryOperators {
+    public static void main(String[] args) {
+        // Java Operators
+        int a = 10, b = 5;
+
+        // 6. Unary Operators
+        int f = 5;
+        System.out.println("Unary plus: " + (+f));        // +
+        System.out.println("Unary minus: " + (-f));       // -
+        System.out.println("Pre-increment: " + (++f));    // ++
+        System.out.println("Post-increment: " + (f++));   // ++
+        System.out.println("Pre-decrement: " + (--f));    // --
+        System.out.println("Post-decrement: " + (f--));   // --
+
+    }
+}
