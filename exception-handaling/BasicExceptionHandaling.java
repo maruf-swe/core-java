9c96a0f9708734c75dad018dc4c1a2bff1225ceb
Maruf <marufur1847@gmail.com>
2025-01-07 23:54:00 +0600

    add exception handaling

---
+++ exception-handaling/BasicExceptionHandaling.java
@@ -0,0 +1,19 @@
+package basic;
+
+public class BasicExceptionHandaling {
+    public static void main(String[] args) {
+        /*
+        1. What is an Exception?
+        An exception is an event that disrupts the normal flow of a program's instructions. It can be caused by various factors, such as incorrect user input, hardware failure, or programming bugs.
+
+        2. Types of Exceptions:
+        Checked Exceptions: Exceptions that are checked at compile-time. The compiler ensures these exceptions are either caught or declared in the method signature.
+                Example: IOException, SQLException
+        Unchecked Exceptions: Exceptions that occur at runtime, and are not checked by the compiler. These are typically caused by programming bugs.
+                Example: NullPointerException, ArithmeticException
+        Errors: These are not exceptions, but serious issues that typically can’t be recovered from. They are subclasses of java.lang.Error.
+                Example: OutOfMemoryError, StackOverflowError
+
+         */
+    }
+}
