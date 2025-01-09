9032ec808a2630e0cca8d63a345003a1b9db171c
Maruf <marufur1847@gmail.com>
2025-01-09 20:44:00 +0600

    add collection

---
+++ collections/BasicsOfCollection.java
@@ -0,0 +1,11 @@
+package collection;
+
+public class BasicsOfCollection {
+    public static void main(String[] args) {
+        /*
+        * Java Collections Framework is a set of classes and interfaces that provide an architecture to
+            store and manipulate groups of objects. It includes a variety of data structures and algorithms
+                to handle common data manipulation tasks such as searching, sorting, insertion, deletion, and more.*/
+
+    }
+}
