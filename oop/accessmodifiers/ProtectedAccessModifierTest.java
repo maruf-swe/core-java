017862bcef4297d476db96417adc8c497b5994f8
Maruf <marufur1847@gmail.com>
2025-01-13 03:34:06 +0600

    protected accessmodifiers update

--- oop/accessmodifiers/ProtectedAccessModifierTest.java
+++ oop/accessmodifiers/ProtectedAccessModifierTest.java
@@ -1,5 +1,14 @@
 package javaoop.accessmodifiers;
+/*
 
+Access Hierarchy for protected:
+In the same class: Accessible.
+In a subclass (in the same package): Accessible.
+In a subclass (in a different package): Accessible.
+In a non-subclass (in the same package): Accessible.
+In a non-subclass (in a different package): Not accessible.
+
+ */
 public class ProtectedAccessModifierTest {
     public static void main(String[] args) {
         ProtectedAccessModifierDog dog = new ProtectedAccessModifierDog("Tommy");
