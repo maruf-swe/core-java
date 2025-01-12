2f32f62de00263c8f7d4bf30367d2b01db597b9c
Maruf <marufur1847@gmail.com>
2025-01-12 23:57:59 +0600

    Parameterized constructor

---
+++ oop/constructors/ParameterizedConstructor.java
@@ -0,0 +1,29 @@
+package javaoop.constructors;
+
+class ParameterizedConstructorExample {
+    int id;       // Instance variable
+    String name;  // Instance variable
+
+    // Parameterized constructor
+    ParameterizedConstructorExample(int id, String name) {
+        this.id = id;       // Use 'this' to refer to instance variables
+        this.name = name;
+        System.out.println("Parameterized Constructor Called!");
+    }
+
+    void display() {
+        System.out.println("ID: " + id + ", Name: " + name);
+    }
+}
+
+public class ParameterizedConstructor {
+    public static void main(String[] args) {
+        // Create objects with different initial values
+        ParameterizedConstructorExample obj1 = new ParameterizedConstructorExample(101, "Alice");
+        ParameterizedConstructorExample obj2 = new ParameterizedConstructorExample(102, "Bob");
+
+        obj1.display(); // Outputs: ID: 101, Name: Alice
+        obj2.display(); // Outputs: ID: 102, Name: Bob
+    }
+}
+
