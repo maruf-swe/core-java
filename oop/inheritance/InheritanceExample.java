fdd1bd7dc686fea971c33889a73e74655a95e63b
Maruf <marufur1847@gmail.com>
2025-01-14 23:59:56 +0600

    inheritance examples

--- oop/inheritance/InheritanceExample.java
+++ oop/inheritance/InheritanceExample.java
@@ -61,6 +61,7 @@ public class InheritanceExample {
     public static void main(String args[]) {
 
         MountainBike mb = new MountainBike(3, 100, 25);
+        
         System.out.println(mb.toString());
     }
 }
