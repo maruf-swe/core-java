f2436790d612d35fef79be400908bcc084dca230
Maruf <marufur1847@gmail.com>
2025-01-15 09:38:51 +0600

    polymorphism

---
+++ oop/polymorphism/Basics.java
@@ -0,0 +1,29 @@
+package javaoop.polymorphism;
+
+public class Basics {
+
+/*
+The word ‘polymorphism’ means ‘having many forms’. In Java, polymorphism refers to the ability of a message to be displayed
+in more than one form. This concept is a key feature of Object-Oriented Programming and it allows objects to behave differently
+ based on their specific class type.
+
+Real-life Illustration of Polymorphism in Java: A person can have different characteristics at the same time. Like a man at
+the same time is a father, a husband, and an employee. So the same person possesses different behaviors in different situations.
+ This is called polymorphism.
+
+                Types of Polymorphism::
+       1. Compile-Time Polymorphism (Static Polymorphism):: Compile-time polymorphism occurs when the method to be
+                 executed is determined at compile time. The most common example is method overloading.
+
+       2. Runtime Polymorphism (Dynamic Polymorphism):: Runtime polymorphism occurs when the method to be executed is
+                              determined at runtime. This is achieved through method overriding.
+
+
+        Advantages of Polymorphism::
+* Increases code reusability by allowing objects of different classes to be treated as objects of a common class.
+* Improves readability and maintainability of code by reducing the amount of code that needs to be written and maintained.
+* Supports dynamic binding, enabling the correct method to be called at runtime, based on the actual class of the object.
+* Enables objects to be treated as a single type, making it easier to write generic code that can handle objects of different types.
+ */
+
+}
