9178662d0841661f7b176ac19d8f28132971f724
Maruf <marufur1847@gmail.com>
2025-01-16 07:11:59 +0600

    multiple interface implement update

--- oop/abstraction/interfaces/MultipleInterfaceImplement.java
+++ oop/abstraction/interfaces/MultipleInterfaceImplement.java
@@ -2,16 +2,22 @@ package javaoop.interfaces;
 
 interface A {
     void start();
+    void area(int l, int h);
 }
 
 interface B {
     void stop();
 }
 
-class c implements A, B {
+class C implements A, B {
     @Override
     public void start() {
         System.out.println("Start engine");
+    }
+
+    @Override
+    public void area(int length, int height) {
+        System.out.println("Rectangle Area is: "+ length * height);
     }
 
     @Override
@@ -22,6 +28,9 @@ class c implements A, B {
 
 public class MultipleInterfaceImplement {
     public static void main(String[] args) {
-
+        C c1 = new C();
+        c1.stop();
+        c1.start();
+        c1.area(4,5);
     }
 }
