4989b025866219c5efa4807b1a4050ee9f0555e2
Maruf <marufur1847@gmail.com>
2025-01-18 13:42:39 +0600

    multiple exception in one

--- exception-handling/MultipleExceptionsInOneCatch.java
+++ exception-handling/MultipleExceptionsInOneCatch.java
@@ -2,6 +2,12 @@ package exceptionhandling;
 
 public class MultipleExceptionsInOneCatch {
     public static void main(String[] args) {
-        
+        try {
+            String str = null;
+            int len = str.length();
+            int result = 10 / 0;
+        } catch (NullPointerException | ArithmeticException e) {
+            System.out.println("Occured NullPointer exception or Arithmetic exception");
+        }
     }
 }
